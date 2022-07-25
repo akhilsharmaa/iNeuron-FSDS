35f87726c82c2d7b72dbb78822ea95ec6ea95bad
“akhilsharmaa” <akhilsharma3332@gmail.com>
2022-07-25 23:59:08 +0530

    Pandas Class

---
+++ Python Programming/15_MongoDb/test.py
@@ -0,0 +1,81 @@
+import pymongo
+
+data =  [
+        {
+            "item": "canvas",
+            "qty": 100,
+            "size": {"h": 28, "w": 35.5, "uom": "cm"},
+            "status": "A",
+        },
+        {
+            "item": "journal",
+            "qty": 25,
+            "size": {"h": 14, "w": 21, "uom": "cm"},
+            "status": "A",
+        },
+        {
+            "item": "mat",
+            "qty": 85,
+            "size": {"h": 27.9, "w": 35.5, "uom": "cm"},
+            "status": "A",
+        },
+        {
+            "item": "mousepad",
+            "qty": 25,
+            "size": {"h": 19, "w": 22.85, "uom": "cm"},
+            "status": "P",
+        },
+        {
+            "item": "notebook",
+            "qty": 50,
+            "size": {"h": 8.5, "w": 11, "uom": "in"},
+            "status": "P",
+        },
+        {
+            "item": "paper",
+            "qty": 100,
+            "size": {"h": 8.5, "w": 11, "uom": "in"},
+            "status": "D",
+        },
+        {
+            "item": "planner",
+            "qty": 75,
+            "size": {"h": 22.85, "w": 30, "uom": "cm"},
+            "status": "D",
+        },
+        {
+            "item": "postcard",
+            "qty": 45,
+            "size": {"h": 10, "w": 15.25, "uom": "cm"},
+            "status": "A",
+        },
+        {
+            "item": "sketchbook",
+            "qty": 80,
+            "size": {"h": 14, "w": 21, "uom": "cm"},
+            "status": "A",
+        },
+        {
+            "item": "sketch pad",
+            "qty": 95,
+            "size": {"h": 22.85, "w": 30.5, "uom": "cm"},
+            "status": "A",
+        },
+    ]
+
+
+client = pymongo.MongoClient("mongodb+srv://akhilsharmaa:54321Start@cluster0.8a8efmd.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+database = client["myInfo"];
+collection = database['akhil'];
+
+# collection.insert_many(data);
+
+# record = collection.find({"status": {'$in':['A', 'P']}})
+# record = collection.find({"status": {'$gt':'C'}})
+record = collection.find({"qty": {'$gte':75}})
+# 
+
+for i in record: 
+    print(i)
